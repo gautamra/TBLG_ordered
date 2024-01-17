11008bb17edaf2c1fe561ef6e63db2c45e2a5b01
GR <gautam.rai@uni-hamburg.de>
2024-01-17 10:19:43 +0100

    initial commit

---
+++ nu_-0.00/CNP_helper.py
@@ -0,0 +1,180 @@
+# Basic functionality
+import numpy as np
+import scipy.linalg as lin
+import math
+from itertools import product
+from h5 import *
+import pickle
+
+# triqs+CTHYB DMFT functionality
+from triqs.gf import *
+from triqs.operators import *
+from triqs_cthyb import Solver
+import triqs
+import triqs.utility.mpi as mpi
+from triqs.operators.util import *
+
+# DFTtools interface
+from triqs_dft_tools.converters.hk import *
+from triqs_dft_tools.sumk_dft import *
+
+# Plotting functionality
+import matplotlib.pyplot as plt
+from triqs.plot.mpl_interface import oplot
+
+#
+import os
+import sys
+
+sig0 = np.eye(2)
+sigx = np.array([[0,1],[1,0]])
+sigy = np.array([[0,-1j],[1j,0]])
+sigz = np.array([[1,0],[0,-1]])
+
+U = 57.95
+W1 = 44.03
+W3 = 50.20
+V = 48.33
+J = 16.38
+
+Boltzmann=8.617333262e-5*1000
+
+def sample_BZ_direct(sample_len):
+    K = 1.703 # 1/Angstrom
+    theta = 1.05 # deg
+    k = 2*K*np.sin(theta/2 * np.pi/180)
+    # Moire reciprocal lattice vectors
+    bM1, bM2 = k*np.array([np.sqrt(3)/2,3/2]), k*np.array([-np.sqrt(3)/2,3/2]) 
+    b0,b1 = 1/(sample_len)*(bM1-bM2)/3, 1/(sample_len)*(2*bM1+bM2)/3
+    b0,b1 = 1/(sample_len)*(bM1+bM2)/3, 1/(sample_len)*(2*bM2-bM1)/3
+    weights = []
+    ks = []
+    shift=0
+    
+    for nn in range(sample_len):
+        for mm in range(sample_len+1 - nn):
+            if mm!=0:
+                ks.append((mm+shift)*b0+((nn)+shift)*b1)
+                ks.append((-mm+shift)*b0+(-(nn)+shift)*b1)
+                if mm==sample_len:
+                    weights.append(1/3)
+                    weights.append(1/3)
+                elif mm+nn==sample_len:
+                    weights.append(1/2)
+                    weights.append(1/2)
+                else:
+                    weights.append(1)
+                    weights.append(1)
+    pi = np.pi
+    R = np.array([[np.cos(pi/3), np.sin(pi/3)],[-np.sin(pi/3), np.cos(pi/3)]])
+    k1s = [R@k for k in ks]
+    k2s = [R@k for k in k1s]
+    weights = weights*3
+    
+    ks = ks+k1s+k2s    
+    ks.append(np.array([0,0]))
+    weights.append(1)
+    
+    da = lin.norm(np.cross(b0,b1))/lin.norm(np.cross(bM1,bM2))
+    weights = da*np.array(weights)
+
+    return ks, weights
+
+def SBhamiltonian(fixed_parameters={}):
+    K = 1.703 # 1/A
+    theta = 1.05 # deg
+    k_theta = 2*K*np.sin(theta/2 * np.pi/180) # 1/A
+    aM = 2*np.pi/(3*k_theta)*2 # A
+    lam = 0.3375*aM # Damping factor for hybridization term
+    
+    # Fixed parameters:
+    M = 3.697
+    vstar = -4303
+    vstarp = 1622
+    gamma = -24.75
+    
+    # 4x4 blocks represent the Gamma_3 c, Gamma1+Gamma2 c, and f electrons in order.
+    # Each 4x4 block is made up of 2x2 blocks representing valley K and K'in order.
+    # The 4x4 blocks are built with np.kron(sig_nu, sig_mu), where 
+    # the first (second) Pauli matrix is in valley (orbital) space.
+    H33 = np.zeros((4,4))       # Gamma_3
+    HMM = M*np.kron(sig0, sigx) # Gamma_1 + Gamma_2
+    Hff = np.zeros((4,4))       # f
+    H3M = lambda kx, ky: vstar*(kx*np.kron(sigz,sig0) + 1j*ky*np.kron(sig0, sigz))
+    H3f = lambda kx, ky: np.exp(-lam**2*(kx**2+ky**2)/2)*( gamma*np.kron(sig0,sig0) + vstarp*(kx*np.kron(sigz,sigx)+ky*np.kron(sig0,sigy)) )
+    HMf = np.zeros((4,4))
+    H = lambda kx, ky: np.block([[H33, H3M(kx,ky), H3f(kx,ky)],
+                                 [H3M(kx,ky).conj().T, HMM, HMf],
+                                 [H3f(kx,ky).conj().T, HMf.conj().T, Hff]])
+    return H
+
+def get_nus(dm):
+    """"
+    returns the fillings of Gamma_3, Gamma_1 + Gamma_2, and f electrons in order
+    """
+    dm = (dm['up']+ dm['down'])
+    nu_c3 = np.trace(dm[:4,:4])
+    nu_c1p2 = np.trace(dm[4:-4,4:-4])
+    nu_f = np.trace(dm[-4:,-4:])
+    return nu_c3-4, nu_c1p2-4, nu_f-4
+
+def mean_field_terms(dm, spin):
+    H33 = 1j*np.zeros((4,4))       
+    HMM = 1j*np.zeros((4,4))  
+    Hff = 1j*np.zeros((4,4))       
+    H3M = 1j*np.zeros((4,4))  
+    H3f = 1j*np.zeros((4,4))  
+    HMf = 1j*np.zeros((4,4))
+
+    nu_c3, nu_c1p2, nu_f = get_nus(dm)
+    dm_spin = (dm[spin]) 
+    Of = dm_spin[-4:,-4:] - 1/2*np.eye(4)
+    Ocp = dm_spin[:4,:4] - 1/2*np.eye(4)
+    Ocpp = dm_spin[4:-4,4:-4]- 1/2*np.eye(4)
+    Ofcp = dm_spin[-4:,:4]
+    Ofcpp = dm_spin[-4:,4:-4]
+    
+    # Hartree W and V terms and residual U Hartree shift
+    Hff += (-3.5*U + nu_c3*W1 + nu_c1p2*W3)*np.eye(4)
+    H33 += nu_f*W1*np.eye(4) + (nu_c3 + nu_c1p2)*V*np.eye(4)
+    HMM += nu_f*W3*np.eye(4) + (nu_c3 + nu_c1p2)*V*np.eye(4)
+    
+    # W Fock term
+    H3f += -W1*Ofcp.T
+    HMf += -W3*Ofcpp.T
+    
+    # J Hartree term
+    J_Hartree_factor = -J*(np.kron(sig0, sig0)-np.kron(sigx,sigx))
+    Hff += J_Hartree_factor*(Ocpp.T)
+    HMM += J_Hartree_factor*(Of.T)
+    # Does not include terms connecting spin sectors, Assumption: there are no spin--off-diagonal terms in the density marix
+    
+    # J Fock term
+    Ofcpp_tot = dm['up'][-4:,4:-4]+dm['down'][-4:,4:-4]
+    V3 = (Ofcpp_tot[1,1] + Ofcpp_tot[2,2])
+    V4 = (Ofcpp_tot[0,0] + Ofcpp_tot[3,3]) 
+    HMf += J*np.diag([V4,V3,V3,V4])
+    
+    H = np.block([[H33, H3M, H3f],
+                [H3M.conj().T, HMM, HMf],
+                [H3f.conj().T, HMf.conj().T, Hff]])
+    return H
+
+def path_BZ(num_ks):
+    K = 1.703 # 1/Angstrom
+    theta = 1.05 # deg
+    k = 2*K*np.sin(theta/2 * np.pi/180)
+    # Moire reciprocal lattice vectors
+    bM1, bM2 = k*np.array([np.sqrt(3)/2,3/2]), k*np.array([-np.sqrt(3)/2,3/2])
+
+    num_points = num_ks
+    K_point = (2*bM1-bM2)/3
+    M_point = (bM1 - bM2)/2
+    Gamma_point = np.zeros(2)
+    dk = lin.norm(K_point - Gamma_point)/num_points
+    ks = np.linspace(K_point, Gamma_point, num_points)[:-1]
+    ks2 = np.linspace(Gamma_point, M_point, int(lin.norm(M_point - Gamma_point)/dk))[:-1]
+    ks3 = np.linspace( M_point,K_point, int(lin.norm(M_point - K_point)/dk))[:-1]
+    BZ_path = np.vstack([ks,ks2,ks3])
+    
+    return BZ_path
---
+++ nu_-0.00/dope_CNP.py
@@ -0,0 +1,264 @@
+from CNP_helper import *
+
+import argparse
+parser=argparse.ArgumentParser()
+parser.add_argument("beta", help="inverse temperature", type=float)
+parser.add_argument("loops", help="number of DMFT loops", type=int)
+parser.add_argument("log_n", help="10^log_n number of QMC cycles", type=int)
+parser.add_argument("nu", help="filling", type=float)
+parser.add_argument("nu_from", help="seed filling", type=float)
+parser.add_argument("--mix", help="0.51-1.0 default:0.9", type = float)
+parser.add_argument("--dm_update_every", help="Number of iterations to keep dm constant default:1", type = int)
+args=parser.parse_args()
+beta = args.beta
+log_n = args.log_n
+loops = args.loops
+nu = args.nu
+nu_from = args.nu_from
+
+###################### Check nu, beta, and pick filename for set of system params
+broken_symm = 'K-IVC'
+Boltzmann=8.617333262e-5*1000
+T=1/(Boltzmann*beta)
+p={}
+p['fit_min_n'] = int(160*T**(-0.9577746167096538))
+p["fit_max_n"] = int(209*T**(-0.767031728744396))
+p["imag_threshold"] = 1e-10
+filename = 'Data/beta_{:.2f}/nu_{:.2f}'.format(beta, nu)
+filename_from = 'Data/beta_{:.2f}/nu_{:.2f}'.format(beta, nu_from)
+if not os.path.isfile(filename_from):
+    raise ValueError("file {} does not exist".format(filename_from))
+################### 
+
+# Not to be touched
+constrain=False
+mix = 0.9
+if args.mix:
+    mix = args.mix
+    print(mix)
+#    filename = filename+"mix_{:.2f}".format(mix)
+sample_len = 9
+BZ_sampling, weights = sample_BZ_direct(sample_len)
+n_iw = 1025
+prec_mu = 0.001
+p["n_cycles"] = 10**log_n//160
+p["length_cycle"] = 1000
+p["n_warmup_cycles"] = 5000
+p["perform_tail_fit"] = True
+p["fit_max_moment"] = 4
+
+#################### Choose the right symmetry-breaking here:
+if broken_symm == 'K-IVC':
+    deg_shells = [[['up_0', 'up_1'],['down_0', 'down_1']]]
+else:
+    raise ValueError("Broken symmetry can only by K-IVC right now.")
+
+
+def set_converter(H):
+    ## Write a converter for dft_tools
+    n_k = len(BZ_sampling)
+    density_required = 12+nu 
+    n_shells = 3
+    n_valleys = 2
+
+    if mpi.is_master_node():
+        with open(filename, "w") as A:
+            A.write(str(n_k)+"\n"+str(density_required)+"\n"+str(n_shells)+"\n")
+            # describe all shells
+            for ish in range(n_shells):
+                A.write("1 "+str(ish)+" 0 "+str(2*n_valleys)+"\n")
+            # describe the correlated shells
+            A.write("1\n")
+            A.write("1 2 0 "+str(2*n_valleys)+" 0 0\n")
+            A.write(str(n_valleys)+" 2"*n_valleys+"\n")
+
+        for ik in range(n_k):
+            with open(filename, "a") as A:
+                for row in H(*BZ_sampling[ik]):
+                    A.write("\n")
+                    for hopping in row:
+                        A.write(str(hopping.real)+" ")
+                A.write("\n")
+                for row in H(*BZ_sampling[ik]):
+                    A.write("\n")
+                    for hopping in row:
+                        A.write(str(hopping.imag)+" ")
+                A.write("\n")
+
+    Converter = HkConverter(filename = filename)
+    Converter.convert_dft_input()
+
+    ## Fix the BZ weights for the converter
+    if mpi.is_master_node():
+        with HDFArchive(filename+'.h5','a') as f:
+            f['dft_input']['bz_weights'] = weights           
+            
+# Initial setup 
+H0 = SBhamiltonian()
+
+H = lambda kx, ky: H0(kx, ky)
+set_converter(H)
+SK = SumkDFT(hdf_file=filename+'.h5',use_dft_blocks=True)
+
+## Check if previous runs exist
+previous_runs = 0
+previous_present = False
+if mpi.is_master_node():
+    if os.path.isfile(filename+'.h5'):
+        with HDFArchive(filename+'.h5','a') as f:
+            if 'dmft_output' in f:
+                ar = f['dmft_output']
+                if 'iterations' in ar:
+                    previous_present = True
+                    previous_runs = ar['iterations']
+            else:
+                f.create_group('dmft_output')
+previous_runs = mpi.bcast(previous_runs)
+previous_present = mpi.bcast(previous_present)
+
+# TODO Save run parameters
+if mpi.is_master_node():
+    with HDFArchive(filename+'.h5', 'a') as ar:
+        ar['dmft_output']['params_%i-%i'%(previous_runs+1, loops+previous_runs+1)] = (loops, p['n_cycles'], mix, constrain)    
+
+for iteration_number in range(1,loops+1):
+    if mpi.is_master_node(): print("Iteration = ", iteration_number)
+
+    if not previous_present and iteration_number==1:
+        dm = 0
+        if mpi.is_master_node():
+            with HDFArchive(filename_from+'.h5', 'r') as ar:
+                iterations = ar['dmft_output']['iterations'] 
+                dm = ar['dmft_output']['dm-%i'%iterations] 
+            with HDFArchive(filename+'.h5', 'a') as ar:
+                ar['dmft_output']['dm-0'] = dm
+                
+    if previous_present and iteration_number==1:
+        dm = 0
+        if mpi.is_master_node():
+            with HDFArchive(filename+'.h5', 'r') as ar:
+                iterations = ar['dmft_output']['iterations']
+                dm = ar['dmft_output']['dm-%i'%iterations]    
+    dm = mpi.bcast(dm)
+    
+    Hmf = {}
+    Hmf['up'] = mean_field_terms(dm, spin='up')
+    Hmf['down'] = mean_field_terms(dm, spin='down')
+    if constrain:
+        Hmf['up'] = mean_field_terms(dm_init, spin='up')
+        Hmf['down'] = mean_field_terms(dm_init, spin='down')
+
+    H = lambda kx, ky: H0(kx, ky) + Hmf['up']
+    set_converter(H)
+    if mpi.is_master_node():
+        # add spin down part
+        with HDFArchive(filename+'.h5','a') as f:
+            f['dft_input']['SP'] = 1
+            n_orb = f['dft_input']['n_orbitals']
+            f['dft_input']['n_orbitals'] = np.concatenate((n_orb, n_orb), axis = 1)
+            proj_mat = f['dft_input']['proj_mat']
+            f['dft_input']['proj_mat'] = np.concatenate((proj_mat, proj_mat), axis = 1)
+            hopping1 = f['dft_input']['hopping']
+            hopping2 = hopping1.copy()
+            for ik in range(len(BZ_sampling)):
+                hopping2[ik,0,:,:] = H0(*BZ_sampling[ik]) + Hmf['down']
+            f['dft_input']['hopping'] = np.concatenate((hopping1, hopping2), axis = 1)
+            
+    SK = SumkDFT(hdf_file=filename+'.h5',use_dft_blocks=True)
+    SK.calculate_diagonalization_matrix(write_to_blockstructure=True)
+
+    n_orb = SK.corr_shells[0]['dim']
+    spin_names = ["up","down"]
+    orb_names = [i for i in range(n_orb)]
+    gf_struct = SK.gf_struct_solver_list[0]
+    Umat, Upmat = U_matrix_kanamori(n_orb=n_orb, U_int=U, J_hund=0)
+    h_int = h_int_density(spin_names, orb_names, map_operator_structure=SK.sumk_to_solver[0], U=Umat, Uprime=Upmat)
+    S = Solver(beta=beta, gf_struct=gf_struct) 
+
+    if not previous_present and iteration_number==1:
+        if mpi.is_master_node():
+            with HDFArchive(filename_from+'.h5', 'r') as ar:
+                iterations = ar['dmft_output']['iterations'] 
+                S.Sigma_iw = ar['dmft_output']['Sigma_iw-%i'%iterations]
+                chemical_potential = ar['dmft_output']['mu-%i'%iterations]
+            with HDFArchive(filename+'.h5', 'a') as ar:
+                ar['dmft_output']['Sigma_iw-0'] = S.Sigma_iw
+                ar['dmft_output']['mu-0'] = chemical_potential
+
+    chemical_potential=0
+    if mpi.is_master_node():
+        with HDFArchive(filename+'.h5', 'r') as ar:
+            S.Sigma_iw = ar['dmft_output']['Sigma_iw-%i'%(iteration_number+previous_runs-1)]
+            chemical_potential = ar['dmft_output']['mu-%i'%(iteration_number+previous_runs-1)]
+    S.Sigma_iw << mpi.bcast(S.Sigma_iw)
+    chemical_potential = mpi.bcast(chemical_potential)
+    SK.set_mu(chemical_potential)
+    
+    SK.deg_shells = deg_shells
+    if mpi.is_master_node():
+        print(("The degenerate shells are ", SK.deg_shells))
+    if iteration_number>1 or previous_present:    
+        SK.symm_deg_gf(S.Sigma_iw,ish=0)
+        
+    SK.set_Sigma([ S.Sigma_iw ])                            # put Sigma into the SumK class
+    chemical_potential = SK.calc_mu(precision = prec_mu , delta = 10)  # find the chemical potential for given density
+    S.G_iw << SK.extract_G_loc()[0]                         # calc the local Green function
+                                                                
+    if mpi.is_master_node():
+        mpi.report("Total charge of Gloc : %.6f"%S.G_iw.total_density())
+        
+    # Calculate new G0_iw to input into the solver:
+    S.G0_iw << S.Sigma_iw + inverse(S.G_iw)
+    S.G0_iw << inverse(S.G0_iw)
+    
+    # Solve the impurity problem:
+    S.solve(h_int=h_int, **p)
+
+    if mpi.is_master_node():
+        mpi.report("Total charge of impurity problem : %.6f"%S.G_iw.total_density())
+        
+    Sigma_symm = S.Sigma_iw.copy()
+    SK.symm_deg_gf(Sigma_symm,ish=0)
+    SK.set_Sigma([Sigma_symm])
+    
+    # Update density matrix
+    if not args.dm_update_every or iteration_number%args.dm_update_every==0:
+        dm = {}
+        dm['up'] = 1j*np.zeros((12,12))
+        dm['down'] = 1j*np.zeros((12,12))
+        for ik in range(len(BZ_sampling)):
+            w = weights[ik]
+            G = SK.lattice_gf(ik)
+            for name, g in G:
+                dm[name] += w*g.density()
+        for name, dens in dm.items():
+            if mpi.is_master_node():
+                mpi.report("Symmetrizing density. The largest imaginary component in the diagonal of the density matrix is {}".format(np.max(np.abs(np.diag(dm[name].imag)))))
+            dm[name] = 1/2*(dm[name] + dm[name].conjugate().T)
+        dm['up'] = 1/2*(dm['up'] + dm['down'])
+        dm['down'] = dm['up'].copy()
+
+    if mpi.is_master_node():
+        with HDFArchive(filename+'.h5','r') as ar:
+            mpi.report("Mixing Sigma and G with factor %s"%mix)
+            S.Sigma_iw << mix * S.Sigma_iw + (1.0-mix) * ar['dmft_output']['Sigma_iw-%i'%(iteration_number+previous_runs-1)]
+            if previous_present or iteration_number>1:
+                S.G_iw << mix * S.G_iw + (1.0-mix) * ar['dmft_output']['G_iw-%i'%(iteration_number+previous_runs-1)]
+            for name, mat in dm.items():
+                dm[name]=mix*mat + (1.0-mix)*ar['dmft_output']['dm-%i'%(iteration_number+previous_runs-1)][name]
+    S.G_iw << mpi.bcast(S.G_iw)
+    S.Sigma_iw << mpi.bcast(S.Sigma_iw)
+    dm = mpi.bcast(dm)
+    
+    if mpi.is_master_node():
+        with HDFArchive(filename+'.h5', 'a') as ar:
+            ar['dmft_output']['iterations'] = iteration_number + previous_runs  
+            ar['dmft_output']['G_0-%i'%(iteration_number + previous_runs)] = S.G0_iw
+            ar['dmft_output']['G_tau-%i'%(iteration_number + previous_runs)] = S.G_tau
+            ar['dmft_output']['G_iw-%i'%(iteration_number + previous_runs)] = S.G_iw
+            ar['dmft_output']['Sigma_iw-%i'%(iteration_number + previous_runs)] = S.Sigma_iw
+            ar['dmft_output']['mu-%i'%(iteration_number + previous_runs)] = chemical_potential
+            ar['dmft_output']['dm-%i'%(iteration_number + previous_runs)] = dm
+        print('saved sigma, G etc')
+    
+    SK.save(['chemical_potential','dc_imp','dc_energ'])
